2f06c56704b69a230abae405275cb16f45c446f9
Berend van Veenendaal <bvl300@few.vu.nl>
2013-06-07 21:16:11 +0000

    simpel java programmatje toegevoegd dat 22 retouneerd

---
+++ returnValue/ReturnValue.java
@@ -0,0 +1,10 @@
+//Just a simple test programm
+//TODO implement a program that just returns a simple value
+//
+public class ReturnValue {
+
+	public static void main(String[] args){
+		System.out.println(""+22);
+	}
+}
+						
