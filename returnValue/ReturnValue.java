c29e5fdf0d760300982cafa9132404845caac6d7
Berend van Veenendaal <bvl300@few.vu.nl>
2013-06-15 15:33:43 +0000

    remove old returnValue

--- returnValue/ReturnValue.java
+++ returnValue/ReturnValue.java
@@ -1,2 +1,15 @@
 //Just a simple test programm
 //TODO implement a program that just returns a simple value
+//
+public class ReturnValue {
+	
+	public static void returnValue(int i){
+		System.out.println(""+i);
+	}
+	
+	
+	public static void main(String[] args){
+		returnValue(Integer.parseInt(args[0]));
+	}
+}
+						
