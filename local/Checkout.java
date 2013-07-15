c352670fd6a669e40c7d9377bfb588ff371df81d
Berend van Veenendaal <bvl300@few.vu.nl>
2013-07-15 21:02:53 +0000

    bezig met log policy

--- local/Checkout.java
+++ local/Checkout.java
@@ -11,10 +11,12 @@ import org.tmatesoft.svn.core.SVNException;
 import org.tmatesoft.svn.core.SVNURL;
 import org.tmatesoft.svn.core.wc.SVNRevision;
 import org.tmatesoft.svn.core.SVNDepth;
+import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
+import org.tmatesoft.svn.core.io.SVNRepository;
 import java.io.IOException;
-import java.io.Writer;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.LineNumberReader;
+import java.io.PrintWriter;
 
 //TODO sort out location of the programm, files and pathnames
 public class Checkout{
@@ -31,26 +33,47 @@ public class Checkout{
 	}
 
 
-	private void updateSVNLog(String projectName, Long rev, File f)throws IOException{
-		Writer output =  new BufferedWriter(new FileWriter(f, true));
-		output.append(""+rev);
-		output.close();
+	private void getSVNLogInfo(File f, long rev,SVNURL url)throws SVNException{
+		SVNRepository repository = SVNRepositoryFactory.create(url);
+
+
 	}
 
-	private void updateLog(String projectName, long rev, String type){
+	/**
+ 	*Updates the log file only when the  new version number is not equal to the 
+	*version number in the log.
+ 	*TODO:Implement better log policy
+ 	*/
+	private void updateSVNLog(String projectName, Long rev, File f,SVNURL url)throws IOException,SVNException{
+		FileReader fr = new FileReader(f);
+		LineNumberReader ln = new LineNumberReader(fr);
+		String s = ln.readLine();
+              	if(s==null || !s.equals(""+rev)){
+			//First clean the log file
+			PrintWriter writer = new PrintWriter(f);
+			writer.print("");
+			writer.append(""+rev+"\n\n");
+			//add the new log content
+			getSVNLogInfo(f,rev, url);
+			writer.close();
+		}
+	}
+
+
+	private void updateLog(String projectName, long rev, String  type, SVNURL url)throws SVNException{
 		String fileName = projectName +"Log.txt";
 		File f = new File(basePath+"/"+fileName);
 		try{
 			f.createNewFile();
 			if("svn".equals(type)){
-                        	updateSVNLog(projectName,rev,f);
+                        	updateSVNLog(projectName,rev,f,url);
 	                }
 
 		}catch(IOException e){
 			 System.out.println(e.getMessage());
 		}
-	
 	}
+
 
 	private void checkoutProject(Node project) throws SVNException{
 		String url, type, projectName,user,pwd;
@@ -63,15 +86,19 @@ public class Checkout{
  			projectName = eElement.getElementsByTagName("name").item(0).getTextContent();
 			user = eElement.getElementsByTagName("user").item(0).getTextContent();
                         pwd = eElement.getElementsByTagName("pwd").item(0).getTextContent();
-			
+		
+			//transfer url into SVNURL
+			SVNURL SVNUrl = SVNURL.parseURIEncoded(url);
+				
 			if(type.equals("svn")){
 				 rev = svnCheckOut(url,projectName,user,pwd);
+				 updateLog(projectName,rev,type,SVNUrl);
+
 			}else if(type.equals("git")){
 				//TODO git check out
 			}else{
 		//		throw SVNException("test");
 			}
-			updateLog(projectName,rev,type);
 		}	
 	}
 
