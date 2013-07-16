2873d2dcf03b25ab7b4a691a5cdde690a4e5125e
Berend van Veenendaal <bvl300@few.vu.nl>
2013-07-16 19:34:24 +0000

    Eerste versie van log werkend

--- local/Checkout.java
+++ local/Checkout.java
@@ -5,6 +5,8 @@ import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 import java.io.File;
+import org.tmatesoft.svn.core.SVNLogEntry;
+import org.tmatesoft.svn.core.SVNLogEntryPath;
 import org.tmatesoft.svn.core.wc.SVNClientManager;
 import org.tmatesoft.svn.core.wc.SVNUpdateClient;
 import org.tmatesoft.svn.core.SVNException;
@@ -13,12 +15,18 @@ import org.tmatesoft.svn.core.wc.SVNRevision;
 import org.tmatesoft.svn.core.SVNDepth;
 import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
 import org.tmatesoft.svn.core.io.SVNRepository;
+import org.tmatesoft.svn.core.wc.SVNWCUtil;
+import org.tmatesoft.svn.core.auth.ISVNAuthenticationManager;
 import java.io.IOException;
 import java.io.FileReader;
 import java.io.LineNumberReader;
 import java.io.PrintWriter;
-
-//TODO sort out location of the programm, files and pathnames
+import java.util.*;
+ 
+//TODO -Log policy
+//     -Revission nr of application to be testedi
+//     -Refactoring
+//     -git
 public class Checkout{
 	String basePath = "../../testApplications/";
 		
@@ -33,10 +41,45 @@ public class Checkout{
 	}
 
 
-	private void getSVNLogInfo(File f, long rev,SVNURL url)throws SVNException{
+	/**
+ 	*NOTE  code based on: http://wiki.svnkit.com/Printing_Out_Repository_History
+ 	*/
+	private void writeLog(Collection logEntries,File f,PrintWriter writer){ 
+		 for (Iterator entries = logEntries.iterator(); entries.hasNext(); ) {
+        		SVNLogEntry logEntry = (SVNLogEntry) entries.next();
+            		writer.println("----------------------------------------------");
+			writer.println ("revision: " + logEntry.getRevision( ) );
+            		writer.println( "author: " + logEntry.getAuthor( ) );
+            		writer.println( "date: " + logEntry.getDate( ) );
+            		writer.println( "log message: " + logEntry.getMessage( ) );
+
+            		if ( logEntry.getChangedPaths( ).size( ) > 0 ) {
+                		writer.println( );
+                		writer.println( "changed paths:" );
+                		Set changedPathsSet = logEntry.getChangedPaths( ).keySet( );
+
+               				for ( Iterator changedPaths = changedPathsSet.iterator( ); changedPaths.hasNext( ); ) {
+                    				SVNLogEntryPath entryPath = ( SVNLogEntryPath ) logEntry.getChangedPaths( ).get( changedPaths.next( ) );
+                    				writer.println( " "
+                            			+ entryPath.getType( )
+                           			+ " "
+                            			+ entryPath.getPath( )
+                            			+ ( ( entryPath.getCopyPath( ) != null ) ? " (from "
+                                    		+ entryPath.getCopyPath( ) + " revision "
+                                    		+ entryPath.getCopyRevision( ) + ")" : "" ) );
+                			}
+            		}
+   		}
+	}
+
+	
+	private void getSVNLogInfo(File f, long rev,SVNURL url,String user, String pwd,PrintWriter writer)throws SVNException{
 		SVNRepository repository = SVNRepositoryFactory.create(url);
-
-
+		ISVNAuthenticationManager authManager = SVNWCUtil.createDefaultAuthenticationManager(user, pwd);
+                repository.setAuthenticationManager(authManager);
+	
+		Collection logEntries = repository.log( new String[] { "" } , null , 0 , -1 , true , true );
+		writeLog(logEntries,f,writer);
 	}
 
 	/**
@@ -44,29 +87,29 @@ public class Checkout{
 	*version number in the log.
  	*TODO:Implement better log policy
  	*/
-	private void updateSVNLog(String projectName, Long rev, File f,SVNURL url)throws IOException,SVNException{
+	private void updateSVNLog(String projectName, Long rev, File f,SVNURL url,String user, String pwd)throws IOException,SVNException{
 		FileReader fr = new FileReader(f);
 		LineNumberReader ln = new LineNumberReader(fr);
 		String s = ln.readLine();
-              	if(s==null || !s.equals(""+rev)){
+             	if(s==null || !s.equals(""+rev)){
 			//First clean the log file
 			PrintWriter writer = new PrintWriter(f);
 			writer.print("");
 			writer.append(""+rev+"\n\n");
 			//add the new log content
-			getSVNLogInfo(f,rev, url);
+			getSVNLogInfo(f,rev, url,user,pwd,writer);
 			writer.close();
 		}
 	}
 
 
-	private void updateLog(String projectName, long rev, String  type, SVNURL url)throws SVNException{
+	private void updateLog(String projectName, long rev, String  type, SVNURL url,String user, String pwd)throws SVNException{
 		String fileName = projectName +"Log.txt";
 		File f = new File(basePath+"/"+fileName);
 		try{
 			f.createNewFile();
 			if("svn".equals(type)){
-                        	updateSVNLog(projectName,rev,f,url);
+                        	updateSVNLog(projectName,rev,f,url,user,pwd);
 	                }
 
 		}catch(IOException e){
@@ -92,7 +135,7 @@ public class Checkout{
 				
 			if(type.equals("svn")){
 				 rev = svnCheckOut(url,projectName,user,pwd);
-				 updateLog(projectName,rev,type,SVNUrl);
+				 updateLog(projectName,rev,type,SVNUrl,user,pwd);
 
 			}else if(type.equals("git")){
 				//TODO git check out
