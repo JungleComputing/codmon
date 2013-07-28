4eb4d63e58ff40cede6f1e944c6279d26c60a9d8
Berend van Veenendaal <bvl300@few.vu.nl>
2013-07-28 14:09:55 +0000

    nu wel GitObject zelf toegevoegd

---
+++ local/GitObject.java
@@ -0,0 +1,33 @@
+/**
+ *@Author bvl300
+ *Class for executing operations on a GIT-repository
+ *
+ * */
+import org.eclipse.jgit.api.*;
+
+public class GitObject{
+	
+	private String url;
+	private String project;
+	private String basePath;
+	
+	/**
+ 	*@author bvl300
+	*Constructor creates a new Git object
+ 	*/ 	
+	public GitObject(String basePath, String project, String url){
+		this.basePath = basePath;
+		this.project = project;
+		this.url = url;
+	}
+
+
+	/**
+ 	*@author bvl300
+ 	*Clones a git object's git  repository
+ 	* */	 
+	public void cloneRepo(){
+		
+	}
+
+}
