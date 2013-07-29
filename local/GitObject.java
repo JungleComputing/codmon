7d81d2663344272fd8a4b05c852ef6908781abbc
Berend van Veenendaal <bvl300@few.vu.nl>
2013-07-29 09:24:36 +0000

    eerste versie git clone werkt nu+naam returnValue1 in init aangepast

--- local/GitObject.java
+++ local/GitObject.java
@@ -3,20 +3,24 @@
  *Class for executing operations on a GIT-repository
  *
  * */
+import java.io.File;
 import java.io.IOException;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.NoFilepatternException;
+import org.eclipse.jgit.api.errors.GitAPIException;
 
 public class GitObject{
 	
 	private String url;
 	private String project;
-	private String basePath;
-	
+	private String basePath;	
+
 	/**
  	*@author bvl300
 	*Constructor creates a new Git object
  	*/ 	
 	public GitObject(String basePath, String project, String url){
-		this.basePath = basePath;
+		this.basePath = basePath+"/"+project;
 		this.project = project;
 		this.url = url;
 	}
@@ -26,8 +30,11 @@ public class GitObject{
  	*@author bvl300
  	*Clones a git object's git  repository
  	* */	 
-	public void cloneRepo(){
-		
+	public void cloneRepo() throws NoFilepatternException,GitAPIException{
+		Git.cloneRepository()
+		.setURI(url)
+		.setDirectory(new File(basePath))
+           	.call();  
 	}
 
 }
