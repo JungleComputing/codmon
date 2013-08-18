0f2999a310ec28b58319e533fc3bde5edaf08d20
Berend van Veenendaal <bvl300@few.vu.nl>
2013-08-18 14:49:19 +0000

    Stats.java moet het stats.sh gaan vervagen

---
+++ local/Stats.java
@@ -0,0 +1,15 @@
+/**
+ *@author bvl300
+ *programm is used to start the codmon programm
+ * */
+public class Stats{
+
+	public Stats(){
+
+	}
+
+
+	public static void main(String argv[]){
+		new Stats();
+	}
+}
