3ef31e8d20539b4fc59268208bbafea2e87bba22
Berend van Veenendaal <bvl300@few.vu.nl>
2013-08-20 11:05:48 +0000

    parameter check ingebouwd

--- local/Stats.java
+++ local/Stats.java
@@ -3,13 +3,25 @@
  *programm is used to start the codmon programm
  * */
 public class Stats{
-
-	public Stats(){
+	
+	private static final String CODMON_HOME = "../../";
+       
+	
+	public Stats(String sensor){
 
 	}
 
 
+
+	/**
+ 	*@author bvl300
+ 	*Checks the paramters and if correct, starts a new stats programm
+ 	* */ 	 
 	public static void main(String argv[]){
-		new Stats();
+		if(argv.length ==1){
+			new Stats(argv[0]);
+		}else{
+			System.out.println("Stats expects 1 sensor as a paramater");
+		}
 	}
 }
