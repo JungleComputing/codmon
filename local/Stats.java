7061472a70cf6cc1d6234d7949c2aa5a30e35b58
Berend van Veenendaal <bvl300@few.vu.nl>
2013-09-09 20:43:31 +0000

    pathnames is environment.properties toegevoegd

--- local/Stats.java
+++ local/Stats.java
@@ -3,12 +3,15 @@
  *programm is used to start the codmon programm
  * */
 public class Stats{
-	
-	private static final String CODMON_HOME = "../../";
        
 	
-	public Stats(String sensor){
+	private void run(String sensor){
 
+	}
+
+	
+	public Stats(String sensor){
+		run(sensor);		
 	}
 
 
@@ -21,7 +24,7 @@ public class Stats{
 		if(argv.length ==1){
 			new Stats(argv[0]);
 		}else{
-			System.out.println("Stats expects 1 sensor as a paramater");
+			System.out.println("Stats expects a sensor as a paramater");
 		}
 	}
 }
