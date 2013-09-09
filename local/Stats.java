10132b173b5ff91a9f27424d170776c5bfeccb23
Berend van Veenendaal <bvl300@few.vu.nl>
2013-09-09 22:00:23 +0000

    basis voor starten neergezet

--- local/Stats.java
+++ local/Stats.java
@@ -1,16 +1,65 @@
+import java.io.File;
+
+
 /**
  *@author bvl300
  *programm is used to start the codmon programm
  * */
 public class Stats{
-       
-	
-	private void run(String sensor){
+       	File dday, dday1,dday2,dday3;
 
+
+	/**
+ 	*@author bvl300
+ 	*Copies the the data from dday to dday1 etc etc
+ 	* */
+	private void copyData(){
+
+	}
+
+
+	/**
+	 *@author bvl300
+	 *Creates the differs directories where the results are stored.
+	 **/ 
+	private void createDdayDirectories(){
+		dday = new File("dday");
+		if(!dday.exists()){dday.mkdir();}
+
+		dday1 = new File("dday1");
+     		if(!dday1.exists()){dday1.mkdir();}
+
+		dday2 = new File("dday2");
+		if(!dday2.exists()){dday2.mkdir();}
+
+		dday3 = new File("dday3");
+		if(!dday3.exists()){dday3.mkdir();}
+	}
+
+
+	private void loadProperties(){
+
+	}
+
+	/**
+ 	*@author bvl300
+ 	*initilezes the program. creates and copy dday files to the right directories
+ 	*/ 	 	
+	private void init(){
+		loadProperties();
+		createDdayDirectories();
+		copyData();
+	}
+
+
+	private void run(String sensor){
+		
+		
 	}
 
 	
 	public Stats(String sensor){
+		init();
 		run(sensor);		
 	}
 
