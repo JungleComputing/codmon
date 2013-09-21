c3d1981b8789298b1c3caeba080b2175a7f417fa
Berend van Veenendaal <bvl300@few.vu.nl>
2013-09-21 10:37:00 +0000

    getClassLoader toegevoegd

--- local/StartUp.java
+++ local/StartUp.java
@@ -1,7 +1,10 @@
 import java.io.File;
 import java.io.IOException;
-
-
+import java.util.ArrayList;
+import java.net.MalformedURLException;
+import java.lang.SecurityException;
+import java.net.URLClassLoader;
+import java.net.URL;
 
 /**
  *@author bvl300
@@ -52,11 +55,44 @@ public class StartUp{
 
 	}
 
+	
+	/**
+ 	*@author bvl300
+	*Returns Classloader that contains the necessary jars
+	**/	 	
+	private ClassLoader getClassLoader(String[] jars) throws MalformedURLException, SecurityException{
+		ArrayList<URL> paths = new ArrayList<URL>();
+
+     		for (String externalJar : jars) {
+         		paths.add(new File(externalJar).toURI().toURL());
+     		}
+     		
+		URL[] urls = paths.toArray(new URL[paths.size()]);
+		return new URLClassLoader(urls);
+	}	
+
+	/**
+	 *@author bvl300
+	 *Loads codmon.jar so I can Use it here
+	 **/
+	private void loadJars(){	
+		String[] jars = new String[1];
+		jars[0] = "codmon.jar";
+		
+		try{
+			getClassLoader(jars);
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	} 
+
+
 	/**
  	*@author bvl300
  	*initilezes the program. creates and copy dday files to the right directories
  	*/ 	 	
 	private void init(){
+		loadJars();
 		loadProperties();
 		File[] directories = createDdayDirectories(4);
 		copyData(directories);
@@ -64,8 +100,7 @@ public class StartUp{
 
 
 	private void run(String sensor){
-		
-		
+		//Stats.main();
 	}
 
 	
