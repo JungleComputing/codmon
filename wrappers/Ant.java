1fe770b2c4a211864c93aedf27d3b7eef4870b86
Berend van Veenendaal <bvl300@few.vu.nl>
2013-10-22 17:30:00 +0000

    Nu wordt de juiste build.xml op de juiste manier aangeroepen. Kijken vanuit wrappers/classes

--- wrappers/Ant.java
+++ wrappers/Ant.java
@@ -1,21 +1,26 @@
 import java.io.File;
 import org.apache.tools.ant.ProjectHelper;
 import org.apache.tools.ant.Project;
+import org.apache.tools.ant.ProjectHelper;
 
 public class Ant{
 	File buildFile;
 	Project project;
 	ProjectHelper projectHelper;
+	String dir;
 
-	public Ant(){
-		buildFile = new File("build.xml");
+	public Ant(String dir){
+		this.dir= dir;
+		buildFile = new File(dir+"/build.xml");
 		project = new Project();
-		project.setUserProperty("antFile",buildFile.getAbsolutePath());
-		project.init();
+		projectHelper = ProjectHelper.getProjectHelper();
+	}
 
-		projectHelper =ProjectHelper.getProjectHelper();
-		project.addReference("ant.projectHelper",projectHelper);
-		projectHelper.parse(project,buildFile);
+	public void init(){;
+		project.setUserProperty("antFile",buildFile.getAbsolutePath());
+                project.init();
+                project.addReference("ant.projectHelper",projectHelper);
+                projectHelper.parse(project,buildFile);
 	}
 
 	public void run(){
