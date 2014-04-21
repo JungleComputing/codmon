c47b6981532eaf9bd29e49fb6e535298cbe677f3
Berend van Veenendaal <bvl300@few.vu.nl>
2014-04-21 09:55:39 +0000

    Basic interface voor versionControl toegevoegd

---
+++ local/checkoutApplications/versionControl.java
@@ -0,0 +1,11 @@
+
+/**
+@author bvl300 
+Basic interface for version control modules
+*/
+public interface versionControl{
+	
+		public void update() throws Exception;
+		
+		public void updateLog() throws Exception;
+}
