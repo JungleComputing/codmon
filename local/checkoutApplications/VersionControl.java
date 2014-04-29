100b66415fd16ba920f11bcbc514c78a96199b1a
Berend van Veenendaal <bvl300@few.vu.nl>
2014-04-29 21:48:25 +0000

    Feedback H3 verwerk + Interface en VersionControlException geimplementeerd

--- local/checkoutApplications/VersionControl.java
+++ local/checkoutApplications/VersionControl.java
@@ -5,7 +5,14 @@ Basic interface for version control modules
 */
 public interface VersionControl{
 	
+		/**
+		*Fetches a repository
+		**/
 		public void update() throws VersionControlException;
 		
+		/**
+		*If there is no logfile it creates the logfile. 
+		*If there is a logfile it updates the existing logfile
+		**/
 		public void updateLog() throws VersionControlException;
 }
